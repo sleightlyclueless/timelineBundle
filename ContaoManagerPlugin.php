71562e06be18382dc94f17a9ef8718eae906f5ed
Sebastian Zill <49412696+sleightlyclueless@users.noreply.github.com>
2024-02-06 18:30:44 +0100

    basic plugin structure

---
+++ ContaoManagerPlugin.php
@@ -0,0 +1,31 @@
+<?php
+
+/**
+ * @package   TimelineBundle
+ * @author    (c) Sebastian Zill
+ * @license   GNU LGPL 3+
+ * @copyright (c) 2024
+ */
+
+// Namespace: Der eindeutige Pfad, der auf diese entsprechende PHP Datei zeigt, damit sie von anderen Orten aus per use statement eindeutig aufgerufen und oder referenziert werden kann. Diese Datei wird von der /src/TimelineBundle/composer.json aufgerufen, die wiederrum durch den /composer.json im root aufgerufen wurde.
+namespace ixtensa\TimelineBundle;
+
+// Um Bundles zu den Contao Core (Standard) Bundles hinzu zu nehmen, dafür gibt es die Contao Funktion "getBundles" vom "BundlePluginInterface". Dieses Interface verwenden wir also nun, indem wir die per Namespace registrierten Bundle "libraries" in diesem Sinne per use statements verwenden. Folgende 4 werden für die Registrierung verwendet:
+use Contao\ManagerPlugin\Bundle\BundlePluginInterface;
+use Contao\ManagerPlugin\Bundle\Parser\ParserInterface;
+use Contao\ManagerPlugin\Bundle\Config\BundleConfig;
+use Contao\CoreBundle\ContaoCoreBundle;
+
+// Lade das BundlePluginInterface von Contao
+class ContaoManagerPlugin implements BundlePluginInterface
+{
+    // Ziehe dir die Bundles und Konfigurationsdateien vom Bundle Parser
+	public function getBundles(ParserInterface $parser)
+	{
+        // Lade unser Bundle in die Config hinzu – allerdings erst nach dem Core Bundle, damit wir keine DCA Konflikte haben
+		return [
+            BundleConfig::create(TimelineBundle::class)
+                ->setLoadAfter([ContaoCoreBundle::class]),
+		];
+	}
+}
---
+++ TimelineBundle.php
@@ -0,0 +1,18 @@
+<?php
+
+/**
+ * @package   TimelineBundle
+ * @author    (c) IXTENSA GmbH & Co. KG Internet und Webagentur -- Sebastian Zill
+ * @license   GNU LGPL 3+
+ * @copyright (c) 2020
+ */
+
+// Namespace: Der eindeutige Pfad, der auf diese entsprechende PHP Datei zeigt, damit sie von anderen Orten aus eindeutig aufgerufen und oder referenziert werden kann. Diese Datei wird mit seinem Namespace von der /src/DependencyInjection/ContaoManagerPlugin.php aufgerufen.
+namespace ixtensa\TimelineBundle;
+
+// Bundle Klasse zum Symfony Kernel hinzufügen
+use Symfony\Component\HttpKernel\Bundle\Bundle;
+
+class TimelineBundle extends Bundle
+{
+}
